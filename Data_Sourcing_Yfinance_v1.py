c3de7602c016e86abde4eed91dfdd2885dbd3ddf
JeremyPapageorges <jeremypapageorges@hotmail.com>
2024-10-29 21:29:22 +0100

    Patch: worked a bit more on the project in general

---
+++ Data_Sourcing_Yfinance_v1.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import yfinance as yf
+
+def download_data_yfinance(ticker, start_date, end_date):
+
+    sp500_data = yf.download(ticker, start=start_date, end=end_date)
+    closing_prices = sp500_data['Close']
+    monthly_closing_prices = closing_prices.resample('M').mean()
+
+    return closing_prices, monthly_closing_prices
+
+
--- Data_Sourcing_v1.py
+++ Data_Sourcing_v1.py
@@ -4,6 +4,7 @@ def download_data(tickers,start,end):
     key = '3d5c0f448ca8947e438c3bd88905e4d2'
     fred = Fred(api_key = key)
     data_dict = {}
+    monthly_avg_dict = {}
     frequency_dict = {}
 
     for tick in tickers:
@@ -15,18 +16,29 @@ def download_data(tickers,start,end):
             series_info = fred.get_series_info(tick)
             frequency_dict[tick] = series_info.get('frequency', 'Unknown')
 
+            #show the frequency of the time series downloaded:
+            print("FREQUENCY OF TICKER:")
+            print(f'{tick} : {frequency_dict[tick]}')
+
         except Exception as e:
             print(f"Error fetching data for {tick}: {e}")
+
+        # Apply resampling where necesaary depending to 
+        # Handle daily frequency
+        if frequency_dict[tick] == 'Daily, Close' or frequency_dict[tick] == 'Daily' :
+            monthly_avg_dict[tick] = data_dict[tick].resample('M').mean()
+        # Handle weekly frequency
+        if frequency_dict[tick] == 'Weekly, Ending Saturday':
+            monthly_avg_dict[tick] = data_dict[tick].resample('M').mean()
+        # Handling monthly frequency
+        if frequency_dict[tick] == 'Monthly':
+            monthly_avg_dict[tick] = data_dict[tick]
+            #adjust the dates in the index to reflect eom
+            monthly_avg_dict[tick].index = monthly_avg_dict[tick].index - pd.offsets.MonthEnd(1)
+
     combined_data = pd.concat(data_dict.values(), axis = 1)
     combined_data = combined_data.rename_axis("Date", axis="index")
+    combined_monthly_data = pd.concat(monthly_avg_dict.values(), axis = 1)
+    combined_monthly_data = combined_monthly_data.rename_axis("Date", axis = 'index')
 
-    # display the frequency information:
-    print("FREQUENCY OF EACH TICKER:")
-    for ticker, freq in frequency_dict.items():
-        print(f"{tick} : {freq}")
-    
-    # create monthly averaged data:
-    monthly_averages = combined_data.resample('M').mean()
-    pct_change_monthly_averages = monthly_averages.pct_change(12)
-
-    return combined_data, monthly_averages, pct_change_monthly_averages
+    return combined_data, combined_monthly_data
